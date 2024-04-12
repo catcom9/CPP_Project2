8706cab007d53724284cd0c7afb78869e3c66311
Cathal Commins <cathal@localhost.localdomain>
2024-04-12 15:15:30 +0100

    Worked on classes and main

--- LM35.cpp
+++ LM35.cpp
@@ -1,6 +1,54 @@
 #include "LM35.h"
 
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+
+//Default Constructor
 LM35::LM35(){
+    srand(time(NULL));
     temperature = 0;
     voltage = 0;
+    adcData = 0;
+    zone = 0;
 }
+
+
+//To tell the sensor what zone its located in
+LM35::LM35(int initial_zone){
+    srand(time(NULL));
+    temperature = 0;
+    voltage = 0;
+    adcData = 0;
+    zone = initial_zone;
+}
+
+//Test constructor
+LM35::LM35(float initial_temp, float initial_volts, int initial_adcData, int initial_zone){
+    srand(time(NULL));
+    temperature = initial_temp;
+    voltage = initial_volts;
+    adcData = initial_adcData;
+    zone = initial_zone;
+}
+
+//Generate the adcData
+int LM35::analogRead(){
+    adcData = rand() % 100 + 30 ;
+    return adcData;
+}
+
+//calculate the temperature and voltage
+void LM35::updateTemp(){
+    voltage = (float)adcData * 5000/1023;
+    temperature = voltage / 10;
+}
+
+//return the temperature
+int LM35::getTemp(){
+    return temperature;
+}
+
+
+
+
--- LM35.h
+++ LM35.h
@@ -1,11 +1,16 @@
 class LM35 {
 private:
-    int temperature;
-    int voltage;
+    float temperature;
+    float voltage;
+    int adcData;
+    int zone;
 
 public:
     LM35();
-    LM35(int, int);
+    LM35(int);
+    LM35(float, float, int, int);
     int analogRead();
-    int display();
+    void updateTemp();
+    int getTemp();
+    void currentStatus();
 };
