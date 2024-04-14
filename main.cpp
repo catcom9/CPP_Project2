a9fdc2271e5e1a00fd1615cb98527679e996cb44
Cathal Commins <cathal@localhost.localdomain>
2024-04-14 16:42:49 +0100

    Work done over weekend

--- main.cpp
+++ main.cpp
@@ -1,20 +1,24 @@
 #include <iostream>
+#include <time.h>
 
 #include "LM35.h"
 #include "HRSR04.h"
 
 #define TEMP_MAX 24
 #define TEMP_MIN 18
-#define DISTANCE_MAX 5
+#define DISTANCE_MAX 500
 
 using namespace std;
 
 int main(){
+    srand(time(NULL));
+
+
     //Array for temp sensors
-    LM35 TempSenors[4] = {LM35(1), LM35(2), LM35(3), LM35(4)};
+    LM35 TempSenors[4];
 
     //Array for Ultra Sonic Sensors. There is 2 per room. I have an array for each zone.
-    HRSR04 UltraSonicSensors[4][2] = {{HRSR04(1), HRSR04(1)}, {HRSR04(2), HRSR04(2)}, {HRSR04(2), HRSR04(2)}, {HRSR04(2), HRSR04(2)}};
+    HRSR04 UltraSonicSensors[4][2];
 
 
     for (int i = 0; i != 4; i++){
@@ -30,6 +34,7 @@ int main(){
         cout << "Zone " << i+1 << ":" << endl;
         cout << "\tCurrent Temperatue: " << TempSenors[i].getTemp() << "\xC2\xB0 C" << endl;
         cout << "\tPerson in room: ";
+        cout << UltraSonicSensors[i][0].getDistance() << " ";
         if (UltraSonicSensors[i][0].getDistance() < DISTANCE_MAX || UltraSonicSensors[i][1].getDistance() < DISTANCE_MAX) {
             cout << "True" << endl;
         } else {
