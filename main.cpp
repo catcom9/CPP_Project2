2a6fc4fd230cb211695ac96a82a8df76fda5ff2d
Cathal Commins <cathal@localhost.localdomain>
2024-04-16 23:10:01 +0100

    Worked on main

--- main.cpp
+++ main.cpp
@@ -13,35 +13,67 @@ using namespace std;
 int main(){
     srand(time(NULL));
 
-
     //Array for temp sensors
-    LM35 TempSenors[4];
+    LM35 TempSenors[4] = {LM35(0 ,0 ,0), LM35(0 ,0 ,0), LM35(0 ,0 ,0), LM35(0 ,0 ,0)};
 
     //Array for Ultra Sonic Sensors. There is 2 per room. I have an array for each zone.
     HRSR04 UltraSonicSensors[4][2];
 
 
+
+    //User Menu Start
+    cout << "Greenhouse information panel. Please Select an option:" << endl;
+    cout << "\t[1] Normal\n\t[2] Test\n\t[3] Exit" << endl;
+
+    int user_option = 0;
+    cout << "Your option: ";
+    cin >> user_option;
+
+    switch (user_option){
+        case 1:
+
+            for (int i = 0; i != 4; i++){
+                //Generate and calculate all data for all sensors
+                cout << TempSenors[i].analogRead() << endl;
+
+                TempSenors[i].updateTemp();
+
+                for (int x = 0; x != 2; x++){
+                    UltraSonicSensors[i][x].analogRead();
+                    UltraSonicSensors[i][x].calcDistance();
+                }
+            }
+
+            break;
+        case 2:
+
+            break;
+        case 3:
+            return 0;
+        default:
+            cout << "Not an option!" << endl;
+            return 0;
+    }
+    //User Menu End
+
+
+    //Printing of zone Data Start
     for (int i = 0; i != 4; i++){
-        //Generate and calculate all data for all sensors
-        cout << TempSenors[i].analogRead() << endl;
-        TempSenors[i].updateTemp();
-        for (int x = 0; x != 2; x++){
-            UltraSonicSensors[i][x].analogRead();
-            UltraSonicSensors[i][x].calcDistance();
-        }
-
-
         cout << "Zone " << i+1 << ":" << endl;
         cout << "\tCurrent Temperatue: " << TempSenors[i].getTemp() << "\xC2\xB0 C" << endl;
         cout << "\tPerson in room: ";
         cout << UltraSonicSensors[i][0].getDistance() << " ";
+
         if (UltraSonicSensors[i][0].getDistance() < DISTANCE_MAX || UltraSonicSensors[i][1].getDistance() < DISTANCE_MAX) {
             cout << "True" << endl;
         } else {
             cout << "False" << endl;
         }
     }
-
+    //Printing of zone Data End
 
 
 }
+
+
+
